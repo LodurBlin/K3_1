face5a183306718ed66fa0ec8b98b954a3d63d94
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-05-29 13:38:45 +0300

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,1 +1,49 @@
+#include "cl_base.h"
+#include "node.h"
+#include <iostream>
 
+void cl_base::add_spinogriz(std::unique_ptr <cl_base>&& spinogriz) {
+
+	spinogriz->p_predok = this;
+	spinogrizi.push_back(std::move(spinogriz));
+
+}
+
+void cl_base::print_hierarchy() const {
+	if (spinogrizi.size() == 0) {
+		return;
+	}
+	std::cout << "\n" << this->ob_name;
+	for (const auto& spin : spinogrizi) {
+		std::cout << " " << (spin)->get_name();
+		}
+	(spinogrizi.back())->print_hierarchy();
+
+}
+
+void cl_base::detdom(cl_base* new_predok) {
+	if (new_predok) {
+		this->p_predok = new_predok;
+		for (auto& spin : p_predok->spinogrizi) {
+			if (spin.get() == this) {
+				new_predok->add_spinogriz(std::move(spin));
+			}
+		}
+	}
+	else this->p_predok = 0;
+}
+cl_base* cl_base::kto_otez() {
+	return p_predok;
+}
+//????????????????????????????????//
+cl_base* cl_base::search(std::string wanted) {
+	for (const auto& spin : spinogrizi) {
+		if ((spin)->get_name == wanted) {
+			return spin;
+			break;
+		}
+		spin -> search(wanted);
+		return nullptr;
+	}
+	
+}
