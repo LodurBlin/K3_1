e7f3d74b72baa3836e1edebba4392d3834dccb5c
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-05-30 19:14:18 +0300

    Update app.h

--- app.h
+++ app.h
@@ -1,1 +1,12 @@
+#ifndef CL_APP_H
+#define CL_APP_H
+#include "cl_base.h"
 
+class cl_app : public cl_base
+{
+public:
+	cl_app() : cl_base("") {};
+	void derevo();
+	void print() const { print_hierarchy(); };
+};
+#endif
