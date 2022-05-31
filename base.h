a9287283589ace29ca310d5176d629c54abe8e1e
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-05-31 17:57:40 +0300

    Update base.h

--- base.h
+++ base.h
@@ -1,38 +1,37 @@
 #ifndef CL_BASE_H
 #define CL_BASE_H
-#include <string>
 #include <vector>
 #include <memory>
 #include <string>
 
-/*
-Расширить функциональность базового класса:
- - метод поиска объекта на дереве иерархии по имени (метод возвращает указатель на найденный объект или nullptr);
- - метод вывода дерева иерархии объектов;
- - метод вывода дерева иерархии объектов и отметок их готовности;
- - метод установки готовности объекта реализовать (доработать) следующим образом.
-*/
+
 class cl_base {
 	std::string ob_name; //наименование
+	int sost = 0; //состояние готовности
 public:
-	cl_base* p_predok{}; //указатель на головной об.
-	std::vector<std::unique_ptr<cl_base>> spinogrizi; //массив указателей на подчиненных
-	cl_base(std::string name, cl_base* p_predok = 0) : ob_name{ name }, p_predok{ p_predok } {}; //парам. конструктор с указ.  на  головной объект и имя объекта (имеет знач по умолч)
+	cl_base* p_predok{}; //указатель на голову
 
-	void set_name(std::string name) { //определение имени
+	std::vector<std::unique_ptr<cl_base>> spinogrizi; //массив указателей
+
+	cl_base(std::string name, cl_base* p_predok = 0) : ob_name{ name }, p_predok{ p_predok } {}; //constr
+
+	void set_name(std::string name) { //opred imeni
 		ob_name = name;
 	}
-	std::string get_name() const { //получение имени
+	std::string get_name() const { //poluch imeni
 		return ob_name;
 	}
-	void add_spinogriz(std::unique_ptr<cl_base>&&); //добавление потомка в иерархию
+	void add_spinogriz(std::unique_ptr<cl_base>&&);
 
-	void print_hierarchy() const; //вывод наим об.
+	void print_hierarchy(int offset); //vivod
 
-	cl_base* kto_otez();//получение указателя на головной объект
+	cl_base* kto_otez(); //poluch ukaz
 
-	cl_base* search(std::string wanted);
-	void move_to(cl_base* destination);//переопределение головного объекта
+	std::unique_ptr<cl_base> uproot(); //pereopred
+	void move_to(cl_base* destination);//drugomu chelu
 
+	cl_base* search(std::string wanted); //poisk ob po imeni
+	void print_sost(); //vivod otmetok
+	void set(int n); //ustanovit otmetku
 };
 #endif
