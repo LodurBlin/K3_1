f7ddc4bfc82173a897f3e4e9dfad35d43739895f
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-01 20:06:36 +0300

    Update app.h

--- app.h
+++ app.h
@@ -1,12 +1,16 @@
 #ifndef CL_APP_H
 #define CL_APP_H
 #include "cl_base.h"
-
-class cl_app : public cl_base
+#include <iostream>
+class cl_app : public cl_base //1 класс
 {
 public:
 	cl_app() : cl_base("") {};
-	void derevo();
-	void print() const { print_hierarchy(0); };
+	void derevo(); //postr
+	void print() { print_hierarchy(0); }; //zapuck
+	void pechat() { 
+		std::cout << "The tree of objects and their readiness\n";
+		print_sost(0); 
+	};
 };
 #endif
