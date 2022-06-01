caccc8316cff2af3a988ca5606f80da6505a0208
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-01 20:07:14 +0300

    Update base.h

--- base.h
+++ base.h
@@ -7,13 +7,14 @@
 
 class cl_base {
 	std::string ob_name; //наименование
-	int sost = 0; //состояние готовности
+	int sost=0; //состояние готовности
+	std::string readiness = " is not ready"; //готовность строковая
 public:
 	cl_base* p_predok{}; //указатель на голову
 
 	std::vector<std::unique_ptr<cl_base>> spinogrizi; //массив указателей
 
-	cl_base(std::string name, cl_base* p_predok = 0) : ob_name{ name }, p_predok{ p_predok } {}; //constr
+	cl_base(std::string name, cl_base* p_predok = 0) : ob_name{ name }, p_predok{ p_predok } {}; //конструктор
 
 	void set_name(std::string name) { //opred imeni
 		ob_name = name;
@@ -31,7 +32,26 @@ public:
 	void move_to(cl_base* destination);//drugomu chelu
 
 	cl_base* search(std::string wanted); //poisk ob po imeni
-	void print_sost(); //vivod otmetok
+
+	void print_sost(int offset); //vivod otmetok
+
 	void set(int n); //ustanovit otmetku
+
+	void preparing();//ввод готовности
 };
 #endif
+/*
+Расширить функциональность базового класса:
+ - метод поиска объекта на дереве иерархии по имени (метод возвращает указатель на найденный объект или nullptr);
+ - метод вывода дерева иерархии объектов;
+ - метод вывода дерева иерархии объектов и отметок их готовности;
+ - метод установки готовности объекта реализовать (доработать) следующим образом.
+
+ Готовность для каждого объекта устанавливается индивидуально. Готовность задается посредством любого отличного от нуля целого числового значения, которое
+присваивается свойству состояния объекта. Объект переводится в состояние готовности, если все объекты вверх по иерархии до корневого включены, иначе установка
+готовности игнорируется.
+При отключении головного, отключаются все объекты от него по иерархии вниз по ветке. Свойству состояния объекта присваивается значение нуль.
+
+
+
+*/
