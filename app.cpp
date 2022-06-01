8f73f119cc74a62e7367f590305faf310469ddcb
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-01 20:06:09 +0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,5 +1,6 @@
 #include "cl_app.h"
-#include "node.h"
+#include "nodes.h"
+#include "cl_base.cpp"
 #include <iostream>
 using namespace std;
 
@@ -8,17 +9,40 @@ void cl_app::derevo() {
 	cin >> root_name;
 	set_name(root_name);
 	cl_base* cur_predok = this;
+	cl_base* root = this;
 	do {
 		string predok, spinogriz;
+		int clas;
 		cin >> predok;
-		cin >> spinogriz;
 		if (predok == "endtree") {
+			root->preparing();
 			break;//prinimaem sost
 		}
+		cin >> spinogriz >> clas;
 		if (predok != cur_predok->get_name()) {
-			cur_predok = (cur_predok)->spinogrizi.back().get();
+			cur_predok = (cur_predok)->search(predok);
 		}
-		(cur_predok)->add_spinogriz(std::unique_ptr<cl_base>(new node(spinogriz)));
+		switch (clas) { //создаем объект нужного класса и запихиваем в дерево
+		case 2:
+			(cur_predok)->add_spinogriz(std::unique_ptr<cl_base>(new _2(spinogriz)));
+				break;
+		case 3:
+			(cur_predok)->add_spinogriz(std::unique_ptr<cl_base>(new _3(spinogriz)));
+				break;
+		case 4:
+			(cur_predok)->add_spinogriz(std::unique_ptr<cl_base>(new _4(spinogriz)));
+				break;
+		case 5:
+			(cur_predok)->add_spinogriz(std::unique_ptr<cl_base>(new _5(spinogriz)));
+				break;
+		case 6:
+			(cur_predok)->add_spinogriz(std::unique_ptr<cl_base>(new _6(spinogriz)));
+				break;
+		default:
+			cout << "фсё";
+				break;
+		}
 	} while (true);
 	cout << root_name;
-}
+};
+
