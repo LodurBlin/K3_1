08b78b02b905d54fd0b9b4633f32704247c7f20a
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-03 01:07:19 +0300

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -21,15 +21,27 @@ void cl_base::print_hierarchy(int offset) {
 	}
 }
 
-cl_base* cl_base::kto_otez() { 
+void cl_base::print_sost(int offset) {
+	std::cout << "\n" << std::string(offset * 4, ' ') << this->ob_name << this->readiness;
+	if (spinogrizi.size() == 0) {
+		return;
+	}
+	for (const auto& spin : spinogrizi) {
+		(spin)->print_sost(++offset);
+		--offset;
+	}
+}
+
+
+cl_base* cl_base::kto_otez() {
 	return p_predok;
 }
-std::unique_ptr<cl_base> cl_base::uproot(){
+std::unique_ptr<cl_base> cl_base::uproot() {
 	assert(p_predok);
 	auto& ch = p_predok->spinogrizi;
 	std::unique_ptr<cl_base> a = nullptr;
-	for (auto& spin:ch){
-		if(this==spin.get()){
+	for (auto& spin : ch) {
+		if (this == spin.get()) {
 			std::swap(a, spin);
 		}
 	}
@@ -37,25 +49,46 @@ std::unique_ptr<cl_base> cl_base::uproot(){
 	return a;
 }
 
-void cl_base::move_to(cl_base* destination){
-	destination -> add_spinogriz(uproot());
+void cl_base::move_to(cl_base* destination) {
+	destination->add_spinogriz(uproot());
 }
 
-cl_base* cl_base::search(std::string wanted){
-	if(this->get_name()==wanted){
+cl_base* cl_base::search(std::string wanted) {
+	if (this->get_name() == wanted) {
 		return this;
 	}
-	for (const auto& spin : spinogrizi){
+	for (const auto& spin : spinogrizi) {
 		auto res = spin->search(wanted);
-		if(res) return res;
+		if (res) return res;
 	}
 	return nullptr;
 }
-cl_base* cl_base::findIP(std::string wanted){ // /o/o1/o2
-	std::string cur_IP;
-	int s = wanted.size();
-	cl_base* root;
-	cl_base* predok;
-	cur_IP = wanted.substr(1, s - 1); //игнорируем первую /
-	return 0;
-};
+
+
+
+void cl_base::set(int n) {
+if ((p_predok==nullptr) or (p_predok->sost != 0)) {
+		sost = n;
+		if (n != 0) {
+			readiness = " is ready";
+		} else {
+			off();
+		}
+	}
+}
+
+void cl_base::preparing() {
+	int s;
+	std::string name;
+	while(std::cin>>name>>s) {
+		this->search(name)->set(s);
+	} 
+}
+
+void cl_base::off(){
+	readiness = " is not ready";
+	sost=0;
+	for (const auto& spin : spinogrizi){
+		spin->off();
+	}
+}
